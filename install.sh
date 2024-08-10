f179ed9b1bdc392e15a12cc45429c309d5512443
Nguyễn Trọng Tuấn <119487916+Trgtuan10@users.noreply.github.com>
2024-08-10 11:55:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!bin/bash
 
-pip install requirements.txt
-git lfs install
 git clone https://huggingface.co/stablediffusionapi/realistic-vision-v51
 git clone https://huggingface.co/lllyasviel/control_v11e_sd15_ip2p
